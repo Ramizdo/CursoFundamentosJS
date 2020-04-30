ba7661106d54beb4b25c8a63c005bdbc3bca9f4f
Douglas Ramirez <dorz.ram@gmail.com>
2020-04-30 04:06:52 -0400

    Funciones

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = "Douglas", edad = 28;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad("Jesus", 35);
+imprimirEdad("Carmen", 65);
+imprimirEdad("Jose", 18);
