dab9457c25e6ae2a6d6d19db76c51aa51df6b9eb
Douglas Ramirez <dorz.ram@gmail.com>
2020-04-30 03:23:29 -0400

    Strings

---
+++ clase2.js
@@ -0,0 +1,16 @@
+var nombre = "Douglas", apellido = "Ramirez";
+
+var nombreEnMayusculas = nombre.toUpperCase();
+
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = nombre + " " + apellido;
+
+var nombreCompleto2 = `${nombre} ${apellido}`;
+
+var str = nombre.charAt(1) + nombre.charAt(2);
+var str2 = nombre.substr(1, 3);
