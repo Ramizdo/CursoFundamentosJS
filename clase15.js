17d9029b6268f72504269e0428713e81dc1179b5
Douglas Ramirez <dorz.ram@gmail.com>
2020-05-05 02:36:45 -0400

    Estructura repetitiva DO-WHILE

---
+++ clase15.js
@@ -0,0 +1,15 @@
+var contador = 0
+
+const llueve = () => Math.random() < 0.25
+
+do {
+    contador += 1
+} while (!llueve())
+
+/* if (contador === 1) {
+    console.log(`Fui a ver si llovia ${ contador } vez`)
+} else {
+    console.log(`Fui a ver si llovia ${ contador } veces`)
+} */
+
+contador === 1 ? console.log(`Fui a ver si llovia ${ contador } vez`) : console.log(`Fui a ver si llovia ${ contador } veces`)
