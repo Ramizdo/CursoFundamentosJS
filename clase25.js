93ce28355e6f4cc638149ab99bbf9654a2351a4b
Douglas Ramirez <dorz.ram@gmail.com>
2020-05-08 19:37:42 -0400

    Tiempo en JavaScript

---
+++ clase25.js
@@ -0,0 +1,12 @@
+//console.log("A")
+
+// SET TIME OUT RECIBE 2 PARAMETROS
+//1 funcion y el delay
+// 2000 milisegundos, equivalen a 2 segundos
+
+setTimeout(() => console.log("B"), 2000)
+
+//console.log("C")
+for (var i = 0; i <= 100000000; i++){
+
+}
