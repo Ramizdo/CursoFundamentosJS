006fa6bed29db1edfd576ae63c659004f52e234c
Douglas Ramirez <dorz.ram@gmail.com>
2020-05-09 01:44:50 -0400

    Multiples promesas en paralelo

---
+++ clase32.js
@@ -0,0 +1,32 @@
+
+const API_URL = "https://swapi.dev/api/";
+const PEOPLE_URL = "people/:id";
+const OPTS = { crossDomain: true }
+
+function obtenerPersonaje (id) {
+    return new Promise((resolve,reject) => {
+        const URL = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+        $
+        .get(URL, OPTS, function (persona){
+            resolve(persona)
+        })
+        .fail(() => reject(id))
+    })
+}
+
+function onError(id){
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
+}
+
+var ids = [1, 2, 3, 4, 5, 6, 7]
+
+/*var promesas = ids.map(function (id) {
+    return obtenerPersonaje(id)
+})*/
+
+var promesas = ids.map (id => obtenerPersonaje(id))
+
+Promise
+    .all(promesas)
+    .then(persona => console.log(persona))
+    .catch(onError)
